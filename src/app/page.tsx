5f20e6647eb058586a30ad1f9d1a21045a9f4c78
Ani Pottammal <ap7564@nyu.edu>
2025-04-08 11:34:44 -0400

    vercel analytics added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/home/hero/Hero";
 import Blurb from "@/components/home/hero/Blurb";
 import LatestProjects from "@/components/home/projects/LatestProjects";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Blurb />
       <LatestProjects />
+      <Analytics />
     </main>
   );
 }
