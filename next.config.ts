29b6c6b4101d4aa49e953363f11644f129bc58e8
Ani Pottammal <ap7564@nyu.edu>
2025-04-08 11:51:13 -0400

    tailwind update attempt #2

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   poweredByHeader: false,
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
